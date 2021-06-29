dccd3896d51f2953d3b190fa67e01e8af8c8b207
zyb88325 <zyb681025@163.com>
2021-06-29 10:23:52 +0800

    优化

--- src/config.php
+++ src/config.php
@@ -5,9 +5,9 @@ return [
     "namespace" => "App\Entities\Base",  //生成类的命名空间
     "output" => BASE_PATH . "app/Entitiy/Base", //生成类输出到什么地方
     "tables" => [
-        ["table" => "user", "PriKey" => "user_id", "EntityName" => "User"],//table=表名 PriKey=主键  EntityName=实体类名称
-        ['table' => 'memorial_member', "PriKey" => "id", "EntityName" => "Memorial"],
-        ['table' => 'tribute', "PriKey" => "id", "EntityName" => "Tribute"],
+        ["table" => "jc_admin", "PriKey" => "id", "EntityName" => "AdminTrait", "type" => "trait"],//table=表名 PriKey=主键  EntityName=实体类名称
+        //['table' => 'memorial_member', "PriKey" => "id", "EntityName" => "Memorial", "type" => "trait"],
+        //['table' => 'tribute', "PriKey" => "id", "EntityName" => "Tribute", "type" => "trait"],
     ],
     'db-components' => [
         'Builder' => Hyperf\Database\Query\Builder::class,
